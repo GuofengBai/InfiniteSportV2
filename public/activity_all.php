731bd4870a66c286d6db3bbdc7b88343c0828b9f
terminuskyuu <cinro9@gmail.com>
2017-04-10 23:31:30 +0800

    导航栏模板加入

---
+++ public/activity_all.php
@@ -0,0 +1,87 @@
+<?php
+
+    session_start();
+
+    //检测是否登录，若没登录则转向登录界面
+    if(!isset($_SESSION['id'])){
+        header("Location:login.html");
+        exit();
+    }
+?>
+    <?php include "head.html";?>
+
+    <div class="main-container">
+        <div class="padding-md">
+            <h2 class="header-text">
+                活动列表
+                <span class="sub-header">
+
+						</span>
+            </h2>
+            <table class="table table-striped" id="dataTable">
+                <thead>
+                <tr>
+                    <th>活动名</th>
+                    <th>开始时间</th>
+                    <th>结束时间</th>
+                    <th>奖励</th>
+                    <th>发起人</th>
+                </tr>
+                </thead>
+                <tbody id="a_list">
+                <tr>
+                    <td><a href="#" style="font-weight: bold"> 活动1</a> </td>
+                    <td>2017-4-8</td>
+                    <td>2017-4-9</td>
+                    <td>100</td>
+                    <td>发起人1</td>
+                </tr>
+
+                </tbody>
+
+            </table>
+            <div>
+                <ul class="pagination" style="margin-left: 35%">
+                    <li class="disabled"><a href="#">&laquo;</a></li>
+                    <li class="active"><a href="#">1</a></li>
+                    <li><a href="#">2</a></li>
+                    <li><a href="#">3</a></li>
+                    <li><a href="#">4</a></li>
+                    <li><a href="#">5</a></li>
+                    <li><a href="#">&raquo;</a></li>
+                </ul>
+            </div>
+        </div><!-- ./padding-md -->
+
+	</div><!-- /main-container -->
+    <?php include "middle.html";?>
+    /*此处写js代码*/
+    <script>
+        $(function()	{
+            $('#dataTable').dataTable();
+        });
+    </script>
+
+    <script>
+        $.ajax("/api/activity/", {
+            type: 'GET',
+            success: function (result) {
+                data=JSON.parse(result);
+                for(i=0;i<data.length;i++){
+                    var tb="<tr>"+
+                        "<td>"+
+                        "<a href=\"activity_specific.php?aid="+data[i].id+"\" style=\"font-weight: bold\">"+data[i].name+"</a>"+
+                        "</td>"+
+                        "<td>"+data[i].start_date+"</td>"+
+                        "<td>"+data[i].end_date+"</td>"+
+                        "<td>"+data[i].bonus+"</td>"+
+                        "<td>"+ "<a href=\"user_specific.php?uid="+data[i].creator+"\"style=\"font-weight: bold\">"+data[i].creator+"</a>"+"</td>"+
+                        "</tr>";
+                    $("#a_list").append(tb);
+                }
+
+            }
+        });
+    </script>
+
+    <?php include "end.html";?>
---
+++ public/activity_new.php
@@ -0,0 +1,129 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: baiguofeng
+ * Date: 2017/4/10
+ * Time: 20:34
+ */
+
+    session_start();
+
+    //检测是否登录，若没登录则转向登录界面
+    if(!isset($_SESSION['id'])){
+        header("Location:login.html");
+        exit();
+    }
+?>
+    <?php include "head.html";?>
+
+    <div class="main-container">
+        <div class="padding-md">
+            <h2 class="header-text">
+                新建活动
+                <span class="sub-header">
+
+						</span>
+            </h2>
+            <div class="smart-widget m-top-lg widget-dark-blue">
+                <div class="smart-widget-header">
+
+                    <span style="font-size: 18px">新建活动详情</span>
+
+                </div>
+                <div class="smart-widget-inner">
+                    <div class="smart-widget-hidden-section">
+                        <ul class="widget-color-list clearfix">
+                            <li style="background-color:#20232b;" data-color="widget-dark"></li>
+                            <li style="background-color:#4c5f70;" data-color="widget-dark-blue"></li>
+                            <li style="background-color:#23b7e5;" data-color="widget-blue"></li>
+                            <li style="background-color:#2baab1;" data-color="widget-green"></li>
+                            <li style="background-color:#edbc6c;" data-color="widget-yellow"></li>
+                            <li style="background-color:#fbc852;" data-color="widget-orange"></li>
+                            <li style="background-color:#e36159;" data-color="widget-red"></li>
+                            <li style="background-color:#7266ba;" data-color="widget-purple"></li>
+                            <li style="background-color:#f5f5f5;" data-color="widget-light-grey"></li>
+                            <li style="background-color:#fff;" data-color="reset"></li>
+                        </ul>
+                    </div>
+                    <div class="smart-widget-body">
+                        <form class="form-horizontal m-top-md">
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label before2">活动名称</label>
+                                <div class="col-sm-10" >
+                                    <input type="text" class="form-control after2" id="aname" required="required" maxlength="10" style="width:250px">
+                                </div>
+                            </div><!-- /form-group -->
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label before2">开始时间</label>
+                                <div class="col-sm-10">
+                                    <input type="date" class="form-control after2" id="astart" required="required" style="width:250px">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label before2">结束时间</label>
+                                <div class="col-sm-10">
+                                    <input type="date" class="form-control after2" id="aend" required="required" style="width:250px">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label before2">活动奖励</label>
+                                <div class="col-sm-10">
+                                    <input type="number" class="form-control after2" required="required" id="abonus"  maxlength="10" style="width:250px">
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <label class="col-sm-2 control-label before2">活动描述</label>
+                                <div class="col-sm-10">
+                                    <textarea class="form-control after2" id="adesc" rows="5" style="width:85%"></textarea>
+                                </div>
+                            </div>
+                            <span style="display: none" id="uid"><?php echo $_SESSION["id"]?></span>
+                            <button type="submit" class="btn btn-success btn-sm short" id="submit" style="margin-left: 80%">提交</button>
+                        </form>
+                    </div>
+                </div><!-- ./smart-widget-inner -->
+            </div><!-- ./smart-widget -->
+        </div><!-- ./padding-md -->
+
+	</div><!-- /main-container -->
+    <?php include "middle.html";?>
+    /*此处写js代码*/
+    <script>
+        id=$("#uid").text();
+        $("#submit").on("click",function () {
+            an=$("#aname").val();
+            ab=$("#abonus").val();
+            sd=$("#astart").val();
+            ed=$("#aend").val();
+            des=$("#adesc").val();
+            val={
+                name:an,
+                bonus:ab,
+                start_date:sd,
+                end_date:ed,
+                description:des
+            };
+            $.ajax("/api/activity/", {
+                type: 'POST',
+                data: val,
+                async:false,
+                datatype:'json',
+                success: function (result) {
+                    data=JSON.parse(result);
+                    if(data.status=="ok"){
+                        window.location.href="my_activity.php";
+                    }else{
+                        alert(data.detail);
+                    }
+
+                }
+            });
+
+        });
+    </script>
+
+    <?php include "end.html";?>
