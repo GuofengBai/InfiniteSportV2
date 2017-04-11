5d49e17eac449e8b7c8ce5524b17945d16d7ef20
terminuskyuu <cinro9@gmail.com>
2017-04-11 13:13:26 +0800

    对齐

--- public/activity_specific.php
+++ public/activity_specific.php
@@ -32,26 +32,34 @@ if(!isset($_SESSION['id'])){
                     <div class="smart-widget-body">
                         <form class="form-horizontal m-top-md">
                             <div class="form-group">
-                                <label class="col-lg-2 control-label before" style="width: 120px">发起人</label>
-                                <label class="col-lg-3 control-label after" ><p style="text-align: left"><a id="acreator" >用户1</a></p></label>
+                                <label class=" control-label before" style="width: 100px">发起人</label>
+                                <label class=" control-label before"  style="width: 40px">&nbsp</label>
+                                <label class=" control-label after"><p style="text-align: left"><a id="acreator" >用户1</a></p></label>
 
                             </div><!-- /form-group -->
 
                             <div class="form-group">
-                                <label class="col-lg-2 control-label before" style="width: 120px">开始时间</label>
-                                <label class="col-lg-4 control-label after"style="width: 150px"><p class="form-control-static" id="sdate" style="text-align: left">2017-4-9</p></label>
-                                <label class="col-lg-2 control-label before" style="width: 120px">结束时间</label>
-                                <label class="col-lg-4 control-label after"style="width: 150px"><p class="form-control-static" id="edate" style="text-align: left">2017-4-10</p></label>
+                                <label class=" control-label before" style="width: 100px">开始时间</label>
+                                <label class=" control-label before"  style="width: 40px">&nbsp</label>
+                                <label class=" control-label after"style="width: 150px"><p class="form-control-static" id="sdate" style="text-align: left">2017-4-9</p></label>
+                                <label class=" control-label before" style="width: 100px">结束时间</label>
+                                <label class=" control-label before"  style="width: 40px">&nbsp</label>
+                                <label class=" control-label after"style="width: 150px"><p class="form-control-static" id="edate" style="text-align: left">2017-4-10</p></label>
                             </div>
 
                             <div class="form-group">
-                                <label class="col-lg-2 control-label before" style="width: 120px">活动奖励</label>
-                                <label class="col-lg-3 control-label after"><p class="form-control-static" id="bonus" style="text-align: left">100</p></label>
+                                <label class="control-label before" style="width: 100px">活动奖励</label>
+                                <label class=" control-label before"  style="width: 40px">&nbsp</label>
+
+                                <label class="control-label after"><p class="form-control-static" id="bonus" style="text-align: left">100</p></label>
+
+
                             </div>
 
                             <div class="form-group">
-                                <label class="col-lg-2 control-label before"  style="width: 120px">活动描述</label>
-                                <label class="col-lg-10 control-label after">
+                                <label class=" control-label before"  style="width: 100px">活动描述</label>
+                                <label class=" control-label before"  style="width: 40px">&nbsp</label>
+                                <label class=" control-label after">
                                     <p class="form-control-static " id="adesc" style="text-align: left" >啊啊啊啊啊啊啊</p>
                                 </label>
                             </div>
